2d05da6b66facb4b03d277c87387b509883779e4
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2023-07-09 21:28:43 +0200

    navbar querie wip

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width = "60rem";
-    document.getElementById("main").style.display = "none";
+// function openNav() {
+//     document.getElementById("mySidebar").style.width = "60rem";
+//     document.getElementById("main").style.display = "none";
 
+//   }
+function openNav() {
+    const sidebar = document.getElementById("mySidebar");
+    if (window.innerWidth < 400) {
+      sidebar.style.width = "100%";
+      sidebar.style.display = "block";
+      sidebar.style.zIndex = "9999"
+    } else {
+      sidebar.style.width = "60rem";
+      document.getElementById("main").style.display = "none";
+    }
   }
+  
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
   function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
-    document.getElementsById("main").style.display = "flex"
+    document.getElementById("main").style.display = "block"
 
   }
